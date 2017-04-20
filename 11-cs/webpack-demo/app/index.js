1aafd7bcca74b166d85cade45a29318d13989282
Daniel Tsui <tsui.daniel@gmail.com>
2017-04-20 14:35:35 +1000

    cutting edge

---
+++ 11-cs/webpack-demo/app/index.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+// var lib = require('./mylib')
+
+console.log(_.sample(['a','b','c']));
+
+console.log('cutting edge webpack2 !!!!')
+
+
+console.log('sdfsdfsf')
+
---
+++ 11-cs/webpack-demo/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+// looks for entry filenames - starting point
+// scan the content for requires and (imports)
+// bundles everything to a destination you specify
+// that bundled file is what you add with a script tag
+
+const config = {
+  entry: {
+    app: './app'
+  },
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    port: 4000
+  }
+}
+
+module.exports = config;
